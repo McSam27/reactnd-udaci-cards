8b9b146fad3a03de7544f670110fadf73b6a7b5d
Sam McCagg <sam.mccagg@ellucian.com>
2018-12-31 00:20:37 -0500

    add project readme. add utils folder with boilerplate API notes.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Udaci Cards
+
+> React Native flashcard quiz application
+
+### api.js
+
+`getDecks`: return all of the decks along with their titles, questions, and answers.
+
+`getDeck`: take in a single id argument and return the deck associated with that id.
+
+`saveDeckTitle`: take in a single id argument and return the deck associated with that id
+
+`addCardToDeck`: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
---
+++ utils/api.js
@@ -0,0 +1,19 @@
+// return all of the decks along with their titles, questions, and answers.
+export const getDecks = () => {
+
+};
+
+// take in a single id argument and return the deck associated with that id
+export const getDeck = (id) => {
+
+};
+
+// take in a single id argument and return the deck associated with that id
+export const saveDeckTitle = () => {
+
+};
+
+// take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
+export const addCardToDeck = (title, card) => {
+
+};
